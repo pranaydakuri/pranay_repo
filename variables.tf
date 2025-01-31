23f041dcf99a8ca5a2240c49a8742b502d87b731
pranaydakuri <dpranayreddy4811@gmail.com>
2025-01-31 16:35:38 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "client_id" {
-  description = "The Azure Client ID"
+  description = "Azure Client ID"
+  type        = string
 }
 
 variable "client_secret" {
-  description = "The Azure Client Secret"
+  description = "Azure Client Secret"
+  type        = string
 }
 
 variable "tenant_id" {
-  description = "The Azure Tenant ID"
+  description = "Azure Tenant ID"
+  type        = string
 }
 
 variable "subscription_id" {
-  description = "The Azure Subscription ID"
+  description = "Azure Subscription ID"
+  type        = string
 }

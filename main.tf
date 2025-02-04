07f6bf4eec75b9dfde010bdab4a06251b39f60cc
pranaydakuri <dpranayreddy4811@gmail.com>
2025-02-04 13:48:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,75 +1,65 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+resource "azurerm_virtual_network" "VNET1" {
+  name                = "VNET1"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+resource "azurerm_subnet" "SUBNET1" {
+  name                 = "SUBNET1"
   resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = azurerm_virtual_network.VNET1.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+resource "azurerm_network_interface" "NF" {
+  name                = "NF"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.SUBNET1.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-vm"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_B1s"
-
-  network_interface_ids = [
-    azurerm_network_interface.example.id
-  ]
-
-  admin_username                  = "adminuser"
-  admin_password                  = "P@ssw0rd1234!"
+resource "azurerm_linux_virtual_machine" "VM1" {
+  name                  = "VM1"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  size                  = "Standard_B1s"
+  admin_username        = "pranayvm"
+  admin_password        = "P@ssw0rd1234!" # Replace with a strong password
   disable_password_authentication = false
+  network_interface_ids = [azurerm_network_interface.NF.id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    name                 = "osdisk1"
+    disk_size_gb         = 30
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
+
+  computer_name = "linuxvm"
+}
+
+output "vm_public_ip" {
+  value = azurerm_network_interface.NF.ip_configuration[0].private_ip_address
 }
